780b6c61502f87be0a8a6fd2b4e3ec4620506ba9
Elaine Zhao <99935579+ElaineZhao92@users.noreply.github.com>
2022-10-09 14:51:03 +0800

    upload lab2

---
+++ lab2/arch/riscv/kernel/trap.c
@@ -0,0 +1,19 @@
+#include"printk.h"
+#include"clock.h"
+
+void trap_handler(unsigned long scause, unsigned long sepc) {
+    // 通过 `scause` 判断trap类型
+    // 如果是interrupt 判断是否是timer interrupt
+    // 如果是timer interrupt 则打印输出相关信息, 并通过 `clock_set_next_event()` 设置下一次时钟中断
+    // `clock_set_next_event()` 见 4.5 节
+    // 其他interrupt / exception 可以直接忽略
+
+    // YOUR CODE HERE
+    printk("kernel is running!\n");
+    // Exception code == 5 <- supervisor time interrupt
+    if(scause >> 63 == 1 && (scause & 0b11111) == 5){
+        //trap && time interrupt
+        printk("[S] Supervisor Mode Timer Interrupt\n");
+        clock_set_next_event();
+    }
+}
