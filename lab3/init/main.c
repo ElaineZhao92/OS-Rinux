6278402a8f4626fdb7c4fc074b7657106beaa319
Elaine Zhao <99935579+ElaineZhao92@users.noreply.github.com>
2022-10-26 09:12:36 +0800

    upload lab3

---
+++ lab3/init/main.c
@@ -0,0 +1,18 @@
+#include "printk.h"
+#include "sbi.h"
+
+extern void test();
+
+int start_kernel() {
+    // puti(2021);
+    // puts(" Hello RISC-V\n");
+    
+    // puti(csr_read(sstatus));
+    // puts(" \n");
+    // csr_write(sscratch,100);
+    // puti(csr_read(sscratch));
+ 
+    test(); // DO NOT DELETE !!!
+
+	return ;
+}
